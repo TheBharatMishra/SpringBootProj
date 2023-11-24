704c3c82b85de4f093067ecc9464031bfdc6b278
TheBharatMishra <mishrabm2004@gmail.com>
2023-11-24 20:26:05 +0530

    started

---
+++ src/test/java/com/parallelintersect/SpringBootProj/SpringBootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parallelintersect.SpringBootProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
